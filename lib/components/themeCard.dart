27a390ec09a289448f5f3013475c47113dcf9748
yangyiyuan5 <yangyiyuan5@jd.com>
2021-12-12 09:56:16 +0800

    feat(index): 首页布局

---
+++ lib/components/themeCard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ThemeCard extends StatefulWidget {
+  const ThemeCard({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _ThemeCardState createState() => _ThemeCardState();
+}
+
+class _ThemeCardState extends State<ThemeCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(
+            "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+            width: 100.0,
+          ),
+          Text(widget.title)
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/index/components/home.dart
+++ lib/pages/index/components/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lunch_tomorrow/components/themeCard.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,33 +11,69 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> viewData = [
-      {"title": "辣椒炒肉"}
+    List viewData = [
+      {
+        "title": "辣椒炒肉",
+        "headImg": "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"
+      },
+      {
+        "title": "杏鲍菇",
+        "headImg": "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"
+      },
+      {
+        "title": "鸡蛋 ",
+        "headImg": "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"
+      },
     ];
 
-    List<Widget> doms = viewData.map((item) => Text(item['title'])).toList();
-    return Container(
-      padding: EdgeInsets.only(left: 16.0, right: 16.0),
-      child: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 48.0, bottom: 48.0),
-                child: Text(
-                  '明天吃什么',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    color: Colors.blue,
-                  ),
+    _mySliverChildBuilderDelegate() {
+      return SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return ThemeCard(title: viewData[index]['title']);
+        },
+        childCount: viewData.length,
+      );
+    }
+
+    _mySliverGridDelegateWithFixedCrossAxisCount() {
+      return SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 5,
+        childAspectRatio: 1.5,
+      );
+    }
+
+    return Material(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverPadding(
+            padding: EdgeInsets.only(top: 48.0, bottom: 48.0),
+            sliver: SliverList(
+                delegate: SliverChildListDelegate(<Widget>[
+              Text(
+                '明天吃什么',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.blue,
                 ),
               ),
-            ],
+            ])),
           ),
-          ElevatedButton(
-            child: Text("马上去选"),
-            onPressed: () {},
+          SliverPadding(
+            padding: EdgeInsets.only(left: 23, right: 30, bottom: 20),
+            sliver: SliverList(
+                delegate: SliverChildListDelegate(<Widget>[
+              ElevatedButton(
+                child: Text("马上去选"),
+                onPressed: () {},
+              ),
+            ])),
+          ),
+          SliverGrid(
+            delegate: _mySliverChildBuilderDelegate(),
+            gridDelegate: _mySliverGridDelegateWithFixedCrossAxisCount(),
           ),
         ],
       ),
